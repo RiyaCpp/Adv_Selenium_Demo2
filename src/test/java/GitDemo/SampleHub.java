151bdeae104a1ffdb351a75b5c16af5e408dcdc6
Pranav <Pranav@192.168.1.5>
2023-09-13 14:57:21 +0530

    Print statement commit and push

--- src/test/java/GitDemo/SampleHub.java
+++ src/test/java/GitDemo/SampleHub.java
@@ -5,6 +5,7 @@ public class SampleHub {
 	public static void main(String[] args) 
 	{
 		System.out.println("Demo for practice commit and push in GitHub");
+		System.out.println("New updated msg");
 
 	}
 
